7e4c4494ba8dc9c389a6d3493506039bcb1ed489
MaksBerkutov <44840100+MaksBerkutov@users.noreply.github.com>
2023-02-24 20:14:43 +0200

    Add Suppor Many Lang, And Resize count point cryptographic

---
+++ Module/Extension/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoApp.Module.Extension
+{
+    public static class StringExtension
+    {
+        public static string Replace(this string str, char[] old, string _new)
+        {
+            foreach (var c in old) str = str.Replace(c.ToString(), _new);
+            return str;
+        }
+    }
+}
--- Module/ViewModel/MainWindowViewModel.cs
+++ Module/ViewModel/MainWindowViewModel.cs
@@ -5,11 +5,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CryptoApp.Module.Extension;
 
 namespace CryptoApp.Module.ViewModel
 {
+    
     public class MainWindowViewModel:Base.ViewModel
     {
+        private static readonly Dictionary<string, string[]> LANG_VALUE = new Dictionary<string, string[]>()
+        {
+            {"En",new string[] {"Result","More Info","Name","Symbol", "Current Price", "Price Change 24h", "Full Info", "Assets","To","Menu", "Base Converter", "Settings", "Max Item", "Finder", //13
+                "Description", "Assets ID", "Current Price", "Website", "Status", "Pegged", "Volume 24 h","Change 1 hours","Change 24 hours","Change 7 day","Created At","Updated At","Circulating supply","Max supply","Market cap",
+                "Total supply","Full diluted market cap","Go To Site","Count Point Min=2 Max=2000","Cryptographic","Lang","Themes","","","","" } },
+              {"Ру",new string[] {"Результат","Подробнее","Название","Символ", "Текущяя цена", "Изменение цены 24h", "Вся информация", "Монеты","К","Меню", "Базовая конвертация", "Настройки", "Максимум элементов", "Поиск", //13
+                "Описание", "Монеты ID", "Текуцяя цена", "Веб сайт", "Статус", "Pegged", "Volume 24 h","Изменения за 1 час","Изменения за 24 час","Изменения за 7 дней","Создано в","Обновленно в","Circulating supply","Max supply","Market cap",
+                "Total supply","Full diluted market cap","На сайт","Количество точек Минимум=2 Максимум=2000","Крипто-график","Язык","Тема","","","","" } },
+        };
+
         public Base.Command GoToSite { get; }
         private ObservableCollection<string> _lang;
         private ObservableCollection<CryptoLogic.AssetsBase> _assetcItems;
@@ -20,9 +32,13 @@ namespace CryptoApp.Module.ViewModel
         private double _convertToCount = 0;
         private double _convertResult = 0;
         private string _finder;
+        private int _countOfPoint = 720;
+        private string _selectedLang = "En";
         private ScottPlot.WpfPlot _plot;
         public MainWindowViewModel(ScottPlot.WpfPlot plot)
         {
+             
+            _lang = new ObservableCollection<string> (LANG_VALUE[_selectedLang]);
             _plot = plot;
             GoToSite = new Base.Command(GoToSiteHandler);
             _assetcItems = new ObservableCollection<CryptoLogic.AssetsBase>(CryptoLogic.CryptingUp.CryptingUpApi.GetAssetsBase(_defaultTop));
@@ -30,10 +46,10 @@ namespace CryptoApp.Module.ViewModel
         }
         private void GoToSiteHandler(object obj)
         {
-            
-            Process.Start($"https://www.coingecko.com/en/coins/{_assetcSelectedItems.Name.ToLower().Replace(" ","-")}");
+            Process.Start($"https://www.coingecko.com/en/coins/{_assetcSelectedItems.Name.ToLower().Replace(new char[] {'[',']' },"").Replace(" ","-")}");
         }
         //async
+       
         public async Task LoadFullInfo()
         {
             if (_assetcSelectedItems != null)
@@ -42,10 +58,11 @@ namespace CryptoApp.Module.ViewModel
         public async Task BuildGraphics()
         {
             if (_assetcSelectedItems == null) return;
-            var points = await CryptoLogic.CryptingUp.CryptingUpApi.GetCryptoPoints(_assetcSelectedItems);
+            var points = await CryptoLogic.CryptingUp.CryptingUpApi.GetCryptoPoints(_assetcSelectedItems,_countOfPoint);
             List<double>x=new List<double>();   
             List<double>y=new List<double>();   
             List<double>y1=new List<double>();   
+           
 
             points.ToList().ForEach(p =>
             {
@@ -62,6 +79,32 @@ namespace CryptoApp.Module.ViewModel
 
         }
         //Object
+        public string[] AllLang => LANG_VALUE.Keys.ToArray();
+        public string SelectedLang
+        {
+            get=> _selectedLang;
+            set
+            {
+
+                Lang = new ObservableCollection<string>(LANG_VALUE[value]);
+                _selectedLang = value;
+            }
+        }
+        public int CountOfPoint
+        {
+            get => _countOfPoint;
+            set
+            {
+                if (value >= 2 && value <= 2000)
+                {
+                    _countOfPoint = value;
+                    BuildGraphics();
+
+                    OnPropertyChanged(nameof(CountOfPoint));
+                }
+                else CountOfPoint = 720;
+            }
+        }
         public CryptoLogic.AssetsFull AssetsFull
         {
             get => _assetsFull;
@@ -103,7 +146,17 @@ namespace CryptoApp.Module.ViewModel
                 }  
             }
         }
-        public ObservableCollection<string> Lang => _lang;
+        public ObservableCollection<string> Lang
+        {
+            get => _lang;
+            set
+            {
+
+                _lang = value;
+                OnPropertyChanged(nameof(Lang));
+                
+            }
+        }
         public ObservableCollection<CryptoLogic.AssetsBase> AssetsItems
         {
             get => _assetcItems;
